3f43901a229caa7f66616c61b30fe576d52e04ef
fanidc7 <fanidc7@gmail.com>
2020-10-09 11:23:52 +0700

    edit smooth

--- scripts/script.js
+++ scripts/script.js
@@ -14,18 +14,18 @@ $('.smooth-scroll')
       // Only prevent default if animation is actually gonna happen
       event.preventDefault();
       $('html, body').animate({
-        scrollTop: target.offset().top - 50
+        scrollTop: target.offset().top -50
       }, 1000, function() {
         // Callback after animation
         // Must change focus!
         var $target = $(target);
         $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
+        // if ($target.is(":focus")) { // Checking if the target was focused
+        //   return false;
+        // } else {
+        //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+        //   $target.focus(); // Set focus again
+        // };
       });
     }
   }
